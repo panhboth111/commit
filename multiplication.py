ed1492c92b804ee403fff7d6ee40310dbbbdae6f
panhboth <neakpanhboth18@kit.edu.kh>
2019-04-03 07:55:46 +0700

    better code for forloop

--- multiplication.py
+++ multiplication.py
@@ -1,5 +1,4 @@
-n = int(input("Input a number: "))
-
-# use for loop to iterate 10 times
-for i in range(1,11):
-   print(n,'x',i,'=',n*i)
+for i in range (1,11):
+    for j in range(1,11):
+        print(i,"x",j,"=",i*j)
+        print("\n")
